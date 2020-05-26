25cc6139655d89e3ec2dd9a351bf739147b96cef
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 02:30:37 +0530

    Yarn and Firebase integration

--- init.sh
+++ init.sh
@@ -1,14 +1,13 @@
 
-git submodule add https://github.com/AnudeepChPaul/auth.learnbase.com.git ./packages/auth.learnbase.com
-git submodule add https://github.com/AnudeepChPaul/learnbase.com.git ./packages/app.learnbase.com
-
-echo -e '\n.gitmodules\n\packages/**\n\n' >> .gitignore
+git submodule add -f https://github.com/AnudeepChPaul/auth.learnbase.com.git ./packages/auth.learnbase.com
+git submodule add -f https://github.com/AnudeepChPaul/learnbase.com.git ./packages/app.learnbase.com
+git submodule add -f https://github.com/AnudeepChPaul/api.learnbase.com.git ./packages/api.learnbase.com
 
 lerna -v || yarn global add lerna
+firebase --version || yarn global add firebase-tools
 
 yarn
 
 lerna init
-
 lerna bootstrap
 
